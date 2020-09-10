8f59e3b0283783d2b744ac607553af5799bc330e
Crawford <crawfordk24@greenhill.org>
2020-09-10 10:27:19 -0500

    9/8 recommit

---
+++ variable&DataTypes.py
@@ -0,0 +1,17 @@
+#Variables
+counter = 100          # An integer assignment
+miles   = 1000.0       # A floating point
+name    = "John"       # A strings
+
+print(counter)
+print(miles)
+print(name)
+
+#Strings
+str = 'Hello World!'
+print(str)          # Prints complete string
+print(str[0])       # Prints first character of the string
+print(str[2:5])     # Prints characters starting from 3rd to 5th
+print(str[2:])      # Prints string starting from 3rd character
+print(str * 2)      # Prints string two times
+print(str + "TEST") # Prints concatenated string
