c61cf1befb40a8c083ce740146be149682ac70f6
Crawford <crawfordk24@greenhill.org>
2020-09-11 12:25:20 -0500

    9/11

---
+++ Program4Datatypes.py
@@ -0,0 +1,17 @@
+f = 13 #float
+s = "Nice Hat!" #string
+b = bool(36) #boolean
+i = 19 #int
+
+print(type(f))
+print(type(s))
+print(type(b))
+print(type(i))
+
+print(i + f)
+print(i - f)
+print(f/i)
+print(i * f)
+
+print(f % i)
+#the modulus is the remainder of the two variables being divided
