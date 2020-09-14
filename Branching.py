471e5fe6ca2e139a1d3dbc2280772bac29020111
Crawford <crawfordk24@greenhill.org>
2020-09-14 11:38:31 -0500

    9/14

---
+++ Branching.py
@@ -0,0 +1,27 @@
+
+# This program is to learn how to branch
+#9/14/2020
+
+#DECLARE Variables
+a=7
+b=13
+c=14
+d=13
+
+if a%2 == 0:
+    print(b + c)
+
+elif b%2 == 0:
+    print(c-d)
+else:
+    print(d*c/a)
+
+    
+#else:
+    #print('a is odd')
+
+
+#if b%2 == 0:
+    #print(c - d)
+#else:
+    #print(d * c/a)
