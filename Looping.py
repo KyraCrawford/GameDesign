8349777605281b93079474df2610a30f532e263a
Crawford <crawfordk24@greenhill.org>
2020-09-18 11:27:54 -0500

    All classwork for 9/18

--- Looping.py
+++ Looping.py
@@ -1,18 +1,4 @@
-# pattern: 1 2 3 4 5 6 7 8 9
-# 1
-#2
-#3
-#4
-#5
-#6
-#7
-#8
-#9
-# 1
-# 2 2
-# ...
-
-import sys
+# Kyra Crawford
 
 numbers = [1,2,3,4,5,6,7,8,9]
 
@@ -32,3 +18,30 @@ for i in numbers:
 
 for i in numbers:
     print((((((str(i)+' ')* i).rjust(25)+ (' ') + (str(i)+' ')* i)))) #full pyramid with middle space
+
+print('The prime numbers between 1 and 72 are:')
+for num in range(1,72):
+   # all prime numbers are greater than 1
+   if num > 1:
+       for i in range(2, num):
+           if (num % i) == 0:
+               break
+       else:
+           print(num)
+top = 5
+for i in range(0, top + 1):
+    for x in range(top - i, 0, -1):
+        print(x, end=' ')
+    print()
+numterms = 9
+a = 0
+b = 1
+sum = 0
+count = 0
+print("Fibonacci Series: ", end = "\n")
+while(count <= numterms):
+  print(sum, end = " ")
+  count += 1
+  a = b
+  b = sum
+  sum = a + b
