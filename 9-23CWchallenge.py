1f1db1204fcba529a349da15ffad08ddb3636fd6
Crawford <crawfordk24@greenhill.org>
2020-09-24 14:38:49 -0500

    extra

--- 9-23CWchallenge.py
+++ 9-23CWchallenge.py
@@ -5,5 +5,9 @@ for row in range(1, rows+1):
     for column in range(1, row + 1):
         print(column, end=' ')
     print("")
+    for column in range(1,row -1):
+        print(column, end=' ')
+    print("")
+
 
 # I am lost on what to do for the left side
