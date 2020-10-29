8001ab0e2f759f72afd5af6e0f9ac5fa20dbb82c
Crawford <crawfordk24@greenhill.org>
2020-10-29 18:35:23 -0500

    small edits

--- FilesHW.py
+++ FilesHW.py
@@ -13,33 +13,34 @@ welcome = print('\nHello,',name,'\n')
 start = input('Would you like to create a file? (y/n) ')
 
 def clear():
-    time.sleep(3)
+    print('\nLoading next page...')
+    time.sleep(2)
     os.system('cls')
 
 def NEWFILE():
     filename = input('What do you want your file to be named? ')
     FILE = open(filename,'w')
-    FILE.write('***THIS IS YOUR FILE***') #default text in your document
+    FILE.write('***THIS IS YOUR FILE***')
     FILE.close()
     print('Your file has been created')
-    clear() #clear screen for neatness
-    INMENU() #return to menu
+    clear()
+    INMENU()
 
 def WRITEFILE():
-    file = input('What file do you want to write in: ') # user gives file name
+    file = input('What file do you want to write in: ')
     if os.path.exists(file):
-        print('\n\n***WARNING*** : If you have already written something in the file, it will be replaced with what you write.') #override warning
-        answer = input("\nIf would like to write in the file, reply with 'y', if you wish to simply ADD something to your file, reply with 'n': ") #asking if there are okay with it
+        print('\n\n***WARNING*** : If you have already written something in the file, it will be replaced with what you write.')
+        answer = input("\nIf would like to write in the file, reply with 'y', if you wish to simply ADD something to your file, reply with 'n': ")
         if answer == 'y':
             words = input('What do you want to write in your file: ')
             PEN = open(file,'w')
-            PEN.write(words)
+            PEN.write(str('\n',words))
             PEN.close()
-            print('Your words are in the file,',file,end='.')
+            print('Your words are in the file.')
             clear()
             INMENU()
         elif answer == 'n':
-            ok = input('\n\n*IMPORTANT* When the menu pops up again, select option 3 to add more to your file. (ok)') # proof of life/reading the info
+            ok = input('\n\n*IMPORTANT* When the menu pops up again, select option 3 to add more to your file. (ok)')
             if ok == 'ok':
                 print('Good! You will be returned to the menu shortly.')
                 time.sleep(2)
@@ -53,11 +54,13 @@ def WRITEFILE():
 def ADDFILE():
     file = input('What file do you want to update: ')
     if os.path.exists(file):
-        words = input('What do you want to add to your file: ') #putting the users input into a variable/object
+        newline = '\n'
+        words = input('What do you want to add to your file: ')
         PENCIL = open(file,'a')
-        PENCIL.append(words) #adding users words to file
+        PENCIL.write(newline)
+        PENCIL.write(words)
         PENCIL.close()
-        print('"',words,end='" was successfully added to the file,',file) #confirmation for user
+        print('"',words,end=' " was successfully added to the file\n')
         clear()
         INMENU()
     else:
@@ -71,7 +74,7 @@ def READFILE():
         EYE = open(file,'r')
         print('This is the contents of the file,',file,end=':\n')
         print(EYE.read())
-        answer = input("Let me know when you'er done reading! (ok): ") #allows for time for user to read if they want
+        answer = input("Let me know when you're done reading! (ok): ")
         if answer == 'ok':
             print('Great! You will be returned to the menu momentarily.')
             clear()
@@ -84,9 +87,9 @@ def READFILE():
 def DELFILE():
     file = input('What file do you want to delete: ')
     if os.path.exists(file):
-        print('Deleting file...') #just aesthetics
+        print('Deleting file...')
         time.sleep(2)
-        os.remove(file) #deleting file
+        os.remove(file)
         print('File deleted')
         clear()
         INMENU()
@@ -100,12 +103,12 @@ def INMENU():
     print('*     ~ADDITIONAL OPTIONS~      *')
     print('*   _ _ _ _ _ _ _ _ _ _ _ _ _   *')
     print('*  |                         |  *')
-    print('*  |       1. DELETE FILE    |  *')
-    print('*  |       2. NEW FILE       |  *')
-    print('*  |       3. ADD TO FILE    |  *')
-    print('*  |       4. WRITE FILE     |  *')
-    print('*  |       5. READ FILE      |  *')
-    print('*  |       6. EXIT           |  *')
+    print('*  |      1. DELETE FILE     |  *')
+    print('*  |      2. NEW FILE        |  *')
+    print('*  |      3. ADD TO FILE     |  *')
+    print('*  |      4. WRITE FILE      |  *')
+    print('*  |      5. READ FILE       |  *')
+    print('*  |      6. EXIT            |  *')
     print('*  |_ _ _ _ _ _ _ _ _ _ _ _ _|  *')
     print('*                               *')
     print('* * * * * * * * * * * * * * * * *')
