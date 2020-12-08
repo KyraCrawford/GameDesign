0d75324c9384daa0c74e7d35ce756bf703ddcdca
KyraCrawford <crawfordfamily2006@mail/com>
2020-12-08 09:55:51 -0600

    final game done

--- clickergame.py
+++ clickergame.py
@@ -6,13 +6,16 @@ import math
 import random
 import os
 import datetime
+leaderboardfile = open('SCORE_RECORDS_CLICK.txt')
 level = ''
 run = False
+menu = True
 black = (0,0,0)
 white = (255,255,255)
 purp = (114, 40, 189)
 result = 0
 timeleft = 0
+leveltime = 0
 pygame.init()
 width = 1000
 height = 800
@@ -56,28 +59,119 @@ def show_time(time):
 def set_background(bg):
     screen.fill(bg)
     pygame.display.flip()
-menu = True
+
 def mainEasy():
     global timeleft
+    global run
+    global level
+    global leveltime
     timeleft = 60
+    leveltime = 60
     level = 'EASY'
     run = True
 def mainMedium():
     global timeleft
+    global run
+    global level
+    global leveltime
     timeleft = 45
+    leveltime = 45
     level = 'MEDIUM'
     run = True
 def mainHard():
     global timeleft
+    global run
+    global level
+    global leveltime
     timeleft = 30
+    leveltime = 30
     level = 'HARD'
     run = True
 def leaderboard():
     os.system('SCORE_RECORDS_CLICK.txt')
 
+def MENU(menu):
+    while menu:
+        #creating the buttons
+        button_1 = pygame.Rect(100,600,200,50)
+        button_2 = pygame.Rect(400,600,200,50)
+        button_3 = pygame.Rect(700,600,200,50)
+        button_4 = pygame.Rect(150,695,365,50)
+        button_5 = pygame.Rect(570,695,170,50)
+        mx, my = pygame.mouse.get_pos()
+        screen.fill(white)
+        # rectangles for buttons
+        pygame.draw.rect(screen, (purp), button_1)
+        pygame.draw.rect(screen, (purp), button_2)
+        pygame.draw.rect(screen, (purp), button_3)
+        pygame.draw.rect(screen, (purp), button_4)
+        pygame.draw.rect(screen, (purp), button_5)
+        text1 = TITLE_FONT.render("CLICKITY CLICK CLICK", 1, black)
+        screen.blit(text1, (int(width/2 - (text1.get_width()/2)), 100))
+        text2 = INS_FONT.render("HOW TO PLAY:", 1, black)
+        screen.blit(text2, (int(width/2 - text2.get_width()/2), 190))
+        text2_0 = INS_FONT.render("Click the screen as many times as possible!", 1, black)
+        screen.blit(text2_0, (int(width/2 - text2_0.get_width()/2), 240))
+        text2_1 = INS_FONT.render("The amount of time you have to click", 1, black)
+        screen.blit(text2_1, (int(width/2 - text2_1.get_width()/2), 290))
+        text2_2 = INS_FONT.render("is determined by the level you pick.", 1, black)
+        screen.blit(text2_2, (int(width/2 - text2_2.get_width()/2), 340))
+        text2_3 = INS_FONT.render("The higher the difficulty, the less time you have.", 1, black)
+        screen.blit(text2_3, (int(width/2 - text2_3.get_width()/2), 390))
+        text2_4 = INS_FONT.render('The score records are kept on the file, "SCORE_RECORDS_CLICK".',1,black)
+        screen.blit(text2_4, (int(width/2 - text2_4.get_width()/2),440))
+        text3 = WORD_FONT.render("EASY", 1, black)
+        screen.blit (text3, (int(width/5 - text3.get_width()/2), 605))
+        text4 = WORD_FONT.render("MEDIUM", 1, black)
+        screen.blit(text4, (int(width/2 - text4.get_width()/2), 605))
+        text5 = WORD_FONT.render("HARD", 1, black)
+        screen.blit(text5, (int(width/1.25 - text5.get_width()/2), 605))
+        text6 = WORD_FONT.render("LEADERBOARD", 1, black)
+        screen.blit(text6, (int(width/3 - text6.get_width()/2), 700))
+        text7 = WORD_FONT.render("EXIT", 1, black)
+        screen.blit(text7, (int(width/1.53 - text7.get_width()/2), 700))
+        pygame.display.update()
+        event = pygame.event.poll()
+            # code behind clicking the buttons
+        if event.type == pygame.MOUSEBUTTONDOWN:
+            if event.button == 1:
+                click = True
+        if button_1.collidepoint((mx,my)):
+            if click:
+                screen.fill(black)
+                pygame.display.update()
+                menu= False
+                mainEasy()
+                pass
+        if button_2.collidepoint((mx,my)):
+            if click:
+                screen.fill(black)
+                pygame.display.update()
+                menu =False
+                mainMedium()
+                pass
+        if button_3.collidepoint((mx,my)):
+            if click:
+                screen.fill(black)
+                pygame.display.update()
+                menu =False
+                mainHard()
+                pass
+        if button_4.collidepoint((mx,my)):
+            if click:
+                pygame.quit()
+                leaderboard()
+                pass
+        if button_5.collidepoint((mx,my)):
+            if click:
+                pygame.quit()
+                pass
+        click = False
+MENU(True)
 while run:
+    MENU(False)
     font = pygame.font.Font(None, 30)
-    text = font.render('Click to start! SEIZURE WARNING! FLASHING LIGHTS!', True, (0, 0, 0), (255, 255, 255))
+    text = font.render('Click to start! SEIZURE WARNING - FLASHING LIGHTS!', True, (0, 0, 0), (255, 255, 255))
     textRect = text.get_rect()
     textRect.centerx = screen.get_rect().centerx
     textRect.centery = screen.get_rect().centery
@@ -87,101 +181,26 @@ while run:
     set_background(background)
     screen.fill(background)
     pygame.display.flip()
-    show_time(timeleft)
-    if result>0 and timeleft>0:
-        timeleft = timeleft-(int(time.time())-timestart)
+    level = level
+    leveltime = leveltime
+    while True:
+        if result == 0:
+            timestart = int(time.time())
         show_time(timeleft)
-    elif result>0 and timeleft<=0:
-        show_result('Your result is: '+str(result)+'. You\'re great!')
-        SCORE(result)
-        break
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        sys.exit()
-    elif pygame.mouse.get_pressed()[0] and event.type == pygame.MOUSEBUTTONDOWN:
-            if result == 0:
-                timestart = int(time.time())
-            result += 1
-            show_result(result)
-while menu:
-    pygame.init()
-    #creating the buttons
-    button_1 = pygame.Rect(100,600,200,50)
-    button_2 = pygame.Rect(400,600,200,50)
-    button_3 = pygame.Rect(700,600,200,50)
-    button_4 = pygame.Rect(150,695,365,50)
-    button_5 = pygame.Rect(570,695,170,50)
-    mx, my = pygame.mouse.get_pos()
-    screen.fill(white)
-    # rectangles for buttons
-    pygame.draw.rect(screen, (purp), button_1)
-    pygame.draw.rect(screen, (purp), button_2)
-    pygame.draw.rect(screen, (purp), button_3)
-    pygame.draw.rect(screen, (purp), button_4)
-    pygame.draw.rect(screen, (purp), button_5)
-    text1 = TITLE_FONT.render("CLICKITY CLICK CLICK", 1, black)
-    screen.blit(text1, (int(width/2 - (text1.get_width()/2)), 100))
-    text2 = INS_FONT.render("HOW TO PLAY:", 1, black)
-    screen.blit(text2, (int(width/2 - text2.get_width()/2), 190))
-    text2_0 = INS_FONT.render("Click the screen as many times as possible!", 1, black)
-    screen.blit(text2_0, (int(width/2 - text2_0.get_width()/2), 240))
-    text2_1 = INS_FONT.render("The amount of time you have to click", 1, black)
-    screen.blit(text2_1, (int(width/2 - text2_1.get_width()/2), 290))
-    text2_2 = INS_FONT.render("is determined by the level you pick.", 1, black)
-    screen.blit(text2_2, (int(width/2 - text2_2.get_width()/2), 340))
-    text2_3 = INS_FONT.render("The higher the difficulty, the less time you have.", 1, black)
-    screen.blit(text2_3, (int(width/2 - text2_3.get_width()/2), 390))
-    text3 = WORD_FONT.render("EASY", 1, black)
-    screen.blit (text3, (int(width/5 - text3.get_width()/2), 605))
-    text4 = WORD_FONT.render("MEDIUM", 1, black)
-    screen.blit(text4, (int(width/2 - text4.get_width()/2), 605))
-    text5 = WORD_FONT.render("HARD", 1, black)
-    screen.blit(text5, (int(width/1.25 - text5.get_width()/2), 605))
-    text6 = WORD_FONT.render("LEADERBOARD", 1, black)
-    screen.blit(text6, (int(width/3 - text6.get_width()/2), 700))
-    text7 = WORD_FONT.render("EXIT", 1, black)
-    screen.blit(text7, (int(width/1.53 - text7.get_width()/2), 700))
-    pygame.display.update()
-    event = pygame.event.poll()
-        # code behind clicking the buttons
-    if event.type == pygame.MOUSEBUTTONDOWN:
-        if event.button == 1:
-            click = True
-    if button_1.collidepoint((mx,my)):
-        if click:
-            screen.fill(black)
-            pygame.display.update()
-            run = True
-            mainEasy()
-            pass
-    if button_2.collidepoint((mx,my)):
-        if click:
-            screen.fill(black)
-            pygame.display.update()
-            run = True
-            mainMedium()
-            pass
-    if button_3.collidepoint((mx,my)):
-        if click:
-            screen.fill(black)
-            pygame.display.update()
-            run = True
-            mainHard()
-            pass
-    if button_4.collidepoint((mx,my)):
-        if click:
-            pygame.quit()
-            leaderboard()
-            pass
-    if button_5.collidepoint((mx,my)):
-        if click:
-            pygame.quit()
-            pass
-    click = False
-    if event.type == pygame.QUIT:
-        sys.exit()
-while run:
-    show_time('0. If you want to play again, restart this application.')
-    event = pygame.event.poll()
-    if event.type == pygame.QUIT:
-        sys.exit()
+        if result>=0 and timeleft>0:
+            timeleft = (int(leveltime))-(int(time.time())-timestart)
+            show_time(timeleft)
+        elif result>=0 and timeleft<=0:
+            show_result('You got this many clicks: '+str(result)+'. GOOD JOB!')
+            SCORE(result)
+            run = False
+            MENU(True)
+            break
+        event = pygame.event.poll()
+        if event.type == pygame.QUIT:
+            run=False
+            sys.exit()
+        elif pygame.mouse.get_pressed()[0] and event.type == pygame.MOUSEBUTTONDOWN:
+                result += 1
+                show_result(result)
+        run = False
